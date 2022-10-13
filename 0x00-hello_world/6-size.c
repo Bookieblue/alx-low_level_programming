320a44709ea44b400067b61f891f8ffb8651c695
Bookieblue <oluwabukola.fasan@gmail.com>
2022-10-13 13:25:01 -0700

    my commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: always
+ */
+int main(void)
+{
+	printf("the size of a char: %i byte(s)\n", sizeof(char));
+	printf("the size of an int: %c byte(s)\n", sizeof(int));
+	printf("the size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("the size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("the size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
+
